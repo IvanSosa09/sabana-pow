ca982074756110ea03e53f2c60a0fa94146022f8
IvanSosa09 <dannielsosa31@gmail.com>
2022-03-18 14:06:39 -0300

    firt commit in sabana pow

---
+++ react/components/LayoutModeSwitcher.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Button } from 'vtex.styleguide'
+import { IconGrid, IconInlineGrid, IconSingleGrid } from 'vtex.store-icons'
+import { useCssHandles } from 'vtex.css-handles'
+import { useState } from 'react'
+
+const CSS_HANDLES = ['layoutSwitcher']
+
+export const LAYOUT_MODE = [
+  {
+    /** This is the single product view on mobile */
+    value: 'normal',
+    label: 'layoutModeSwitcher.normal',
+  },
+  {
+    /** This is the grid product view on mobile */
+    value: 'small',
+    label: 'layoutModeSwitcher.small',
+  },
+  {
+    value: 'inline',
+    label: 'layoutModeSwitcher.inline',
+  },
+]
+
+const LayoutIcon = ({ mode }) => {
+  switch (mode) {
+    case 'small':
+      return <IconSingleGrid size={20} />
+    case 'inline':
+      return <IconInlineGrid size={20} />
+    case 'normal':
+      return <IconGrid size={20} />
+    default: {
+      // eslint-disable-next-line no-undef
+      if (process.env.NODE_ENV === 'development') {
+        console.warn(`Unsupported icon ${mode} in LayoutIcon`)
+      }
+      return null
+    }
+  }
+}
+
+const LayoutModeSwitcher = ({ activeMode, onChange }) => {
+  const handles = useCssHandles(CSS_HANDLES)
+
+  const [classDark, setClassDark] = ["vtex-flex-layout-0-x-flexCol--darkMode-Col-1"]
+  handleDark = () =>{
+    setClassDark("vtex-flex-layout-0-x-flexCol--darkMode-Col-dark-1")
+  }
+  return (
+    <div>
+      <div className={classDark} onClick={handleDark}>
+        <div className="vtex-flex-layout-0-x-flexColChild--darkMode-Col-1">icon 1 </div>
+        <div className="vtex-flex-layout-0-x-flexColChild--darkMode-Col-1">icon 2</div>
+      </div>
+
+      <div
+      className={`${handles.layoutSwitcher} h-100 flex justify-center items-center pl4`}
+    >
+      <Button
+        variation="tertiary"
+        size="small"
+        onClick={e => onChange(e, activeMode)}
+      >
+        <span className="c-on-base">
+          <LayoutIcon mode={activeMode} />
+        </span>
+      </Button>
+    </div>
+    </div>
+  )
+}
+
+LayoutModeSwitcher.propTypes = {
+  /** Current active mode */
+  activeMode: PropTypes.string,
+  /** On change callback */
+  onChange: PropTypes.func.isRequired,
+}
+
+export default LayoutModeSwitcher
